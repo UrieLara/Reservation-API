82f8905de7ff61abfe0e2a9ec1699a509dc4ca8d
Uriel Lara <141281252+UrieLara@users.noreply.github.com>
2026-05-08 18:29:22 -0300

    merge modulo2

--- model/enums/ReservationStatus.java
+++
@@ -1,5 +0,0 @@
-package com.reservation.model.enums;
-
-public enum ReservationStatus {
-    ACTIVE, CANCELLED
-}
--- model/enums/Role.java
+++
@@ -1,5 +0,0 @@
-package com.reservation.model.enums;
-
-public enum Role {
-    ADMIN, USER
-}
